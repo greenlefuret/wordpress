c7d255f673a6b93f79072702d4a7856fb1cf8e55
Pascal <pmartinez31140@gmail.com>
2018-02-20 13:21:27 +0100

    structure

---
+++ index.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+        <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+
+        <title>Index</title>
+    </head>
+    <body>
+
+        <!--Dropdown debut-->
+        <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+          <ul class="navbar-nav">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="pages/pages1.php">Page 1</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Link</a>
+            </li>
+
+          </ul>
+        </nav>
+    	<!--Dropdown FIN-->
+<div class="container-fluid">
+
+    <div class="row">
+
+        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel" >
+            <ol class="carousel-indicators">
+                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+            </ol>
+            <div class="carousel-inner" role="listbox">
+                <div class="carousel-item active">
+
+                    <img class="d-block img-fluid " src="Images/pay1.jpeg" alt="responsive image First slide" >
+                    <div class="carousel-caption d-none d-md-block">
+                        <h3>Welcome to the Modern University.</h3>
+                        <p>Any prominent career starts with good education. Together with us, you will have an opportunity of getting better and deeper knowledge of the subjects that can build your future</p>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block img-fluid " src="Images/pay2.jpeg" alt="Second slide" >
+                    <div class="carousel-caption d-none d-md-block">
+                        <h3>Different Kinds of Activities</h3>
+                        <p>Take part in numerous amazing activities that will develop you as a person and help you make new friends that will undoubtedly become a part of your student life.</p>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block img-fluid" src="Images/pay3.jpeg" alt="Third slide">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h3>Building the Foundation for Your Future</h3>
+                        <p>At Modern University, you can succeed in lots of research areas and invest in your education as well as gain skills that will help you become an experienced and demanded specialist.</p>
+                    </div>
+                </div>
+            </div>
+            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
+    </div>
+</div>
+<div class="part2">
+    <div class="container">
+        <div class="row">
+            <div class="video col-6">
+
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/NS1QdFNC0RE" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+
+
+
+
+
+            </div>
+            <div class="Texte col-6">
+                <h4>About Our University</h4>
+                <p>As one of the world's premier academic and research institutions, the Modern University has driven new ways of thinking since our founding. Today, we represent an intellectual destination that draws inspired scholars to our local and international campuses, keeping us at the nexus of ideas that challenge and change the world and provide companies with new specialists.</p>
+            </div>
+
+        </div>
+
+    </div>
+
+</div>
+
+
+
+    </body>
+</html>
